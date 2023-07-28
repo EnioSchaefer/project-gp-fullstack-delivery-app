b8103878de673df84617cca3ef531a2823777385
Enio Schaefer <enio100schaefer@hotmail.com>
2023-07-28 00:52:26 -0300

    fix: corrige erros no docker-compose e dockerfiles

--- front-end/src/Components/ProductList.jsx
+++ front-end/src/Components/ProductList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getProductsCart, removeProduct } from '../Utils/LocalStorage';
+import Checkout from './Checkout';
 
 export default function ProductList() {
   const [cartProduct, setCartProducts] = useState([]);
@@ -8,95 +9,95 @@ export default function ProductList() {
     setCartProducts(cart);
   }, []);
 
-  const sumPrices = cartProduct
-    .reduce((acc, curr) => (curr.price * curr.quantity) + acc, 0);
-
   return (
-    <div className="flex justify-center w-full">
-      <div className="py-3">
-        <h1 className="text-center text-4xl mb-4 p-4 bg-indigo-200 rounded-full">Verifique os dados do seu pedido</h1>
-        <table className="border-2 shadow">
-          <thead className="bg-white border-b">
-            <tr>
-              <th className="font-medium text-gray-900 px-6 py-4 text-left">Item</th>
-              <th className="font-medium text-gray-900 px-6 py-4 text-left">Descrição</th>
-              <th className="font-medium text-gray-900 px-6 py-4 text-left">Quantidade</th>
-              <th className="font-medium text-gray-900 px-6 py-4 text-left">Valor Unitário</th>
-              <th className="font-medium text-gray-900 px-6 py-4 text-left">Sub-Total</th>
-              <th className="font-medium text-gray-900 px-6 py-4 text-left">Remover</th>
-            </tr>
-          </thead>
-          <tbody className="bg-white">
-            {cartProduct.map((product, index) => (
-              <tr key={ product.id } className="bg-gray-100 border-b-0">
-                <td
-                  data-testid={
-                    `customer_checkout__element-order-table-item-number-${index}`
-                  }
-                  className="px-6 py-4 whitespace-nowrap font-medium text-gray-900 text-center bg-gray-100"
-                >
-                  {index + 1}
-                </td>
-
-                <td
-                  data-testid={
-                    `customer_checkout__element-order-table-name-${index}`
-                  }
-                  className="text-gray-900 font-light px-6 py-4 whitespace-nowrap bg-white"
-                >
-                  {product.productName}
-                </td>
-
-                <td
-                  data-testid={
-                    `customer_checkout__element-order-table-quantity-${index}`
-                  }
-                  className="text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center bg-gray-100"
-                >
-                  {product.quantity}
-                </td>
-
-                <td
-                  data-testid={
-                    `customer_checkout__element-order-table-unit-price-${index}`
-                  }
-                  className="text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center bg-white"
-                >
-                  R$
-                  {' '}
-                  {(product.price).replace('.', ',')}
-                </td>
-
-                <td
-                  data-testid={
-                    `customer_checkout__element-order-table-sub-total-${index}`
-                  }
-                  className="text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center bg-gray-100"
-                >
-                  R$
-                  {' '}
-                  {(product.price * product.quantity).toFixed(2).replace('.', ',')}
-                </td>
-
-                <td
-                  data-testid={ `customer_checkout__element-order-table-remove-${index}` }
-                  className="bg-white"
-                >
-                  <div className="flex justify-center align-center">
-                    <button
-                      type="button"
-                      onClick={ () => { setCartProducts(removeProduct(product.id)); } }
-                      className="w-20 h-8 text-white bg-red-600 rounded-full baseline hover:bg-red-500"
-                    >
-                      Eliminar
-                    </button>
-                  </div>
-                </td>
+    <>
+      <div className="flex justify-center w-full">
+        <div className="py-3">
+          <h1 className="text-center text-4xl mb-4 p-4 bg-indigo-200 rounded-full">Verifique os dados do seu pedido</h1>
+          <table className="border-2 shadow">
+            <thead className="bg-white border-b">
+              <tr>
+                <th className="font-medium text-gray-900 px-6 py-4 text-left">Item</th>
+                <th className="font-medium text-gray-900 px-6 py-4 text-left">Descrição</th>
+                <th className="font-medium text-gray-900 px-6 py-4 text-left">Quantidade</th>
+                <th className="font-medium text-gray-900 px-6 py-4 text-left">Valor Unitário</th>
+                <th className="font-medium text-gray-900 px-6 py-4 text-left">Sub-Total</th>
+                <th className="font-medium text-gray-900 px-6 py-4 text-left">Remover</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="bg-white">
+              {cartProduct.map((product, index) => (
+                <tr key={ product.id } className="bg-gray-100 border-b-0">
+                  <td
+                    data-testid={
+                      `customer_checkout__element-order-table-item-number-${index}`
+                    }
+                    className="px-6 py-4 whitespace-nowrap font-medium text-gray-900 text-center bg-gray-100"
+                  >
+                    {index + 1}
+                  </td>
+
+                  <td
+                    data-testid={
+                      `customer_checkout__element-order-table-name-${index}`
+                    }
+                    className="text-gray-900 font-light px-6 py-4 whitespace-nowrap bg-white"
+                  >
+                    {product.productName}
+                  </td>
+
+                  <td
+                    data-testid={
+                      `customer_checkout__element-order-table-quantity-${index}`
+                    }
+                    className="text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center bg-gray-100"
+                  >
+                    {product.quantity}
+                  </td>
+
+                  <td
+                    data-testid={
+                      `customer_checkout__element-order-table-unit-price-${index}`
+                    }
+                    className="text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center bg-white"
+                  >
+                    R$
+                    {' '}
+                    {(product.price).replace('.', ',')}
+                  </td>
+
+                  <td
+                    data-testid={
+                      `customer_checkout__element-order-table-sub-total-${index}`
+                    }
+                    className="text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center bg-gray-100"
+                  >
+                    R$
+                    {' '}
+                    {(product.price * product.quantity).toFixed(2).replace('.', ',')}
+                  </td>
+
+                  <td
+                    data-testid={ `customer_checkout__element-order-table-remove-${index}` }
+                    className="bg-white"
+                  >
+                    <div className="flex justify-center align-center">
+                      <button
+                        type="button"
+                        onClick={ () => { setCartProducts(removeProduct(product.id)); } }
+                        className="w-20 h-8 text-white bg-red-600 rounded-full baseline hover:bg-red-500"
+                      >
+                        Eliminar
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+      <Checkout />
+    </>
   );
 }
--- front-end/src/Pages/CheckoutPage.jsx
+++ front-end/src/Pages/CheckoutPage.jsx
@@ -3,7 +3,6 @@ import { Redirect } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
 import { getUser } from '../Utils/LocalStorage';
 import ProductList from '../Components/ProductList';
-import Checkout from '../Components/Checkout';
 
 function CheckoutPage() {
   const [isLogged, setIsLogged] = useState(false);
@@ -18,7 +17,6 @@ function CheckoutPage() {
       {isLogged && <Redirect to="/login" />}
       <Navbar />
       <ProductList />
-      <Checkout />
     </div>
   );
 }
